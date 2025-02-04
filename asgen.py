9e98b6074776919b529cf3cede5b2da61db8e791
Lee Ballard <ballle98@gmail.com>
2025-02-04 17:03:31 -0600

    fix pylint issues Fixes #14

--- asgen.py
+++ asgen.py
@@ -34,6 +34,15 @@ import subprocess
 
 
 def main():
+    """
+    The main function of the program. It takes command line arguments and processes them.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     parser = argparse.ArgumentParser(description='Amazon Statement Generator')
     parser.add_argument('--verbose', '-v', action='count')
     parser.add_argument('filenames', nargs='*')
@@ -46,78 +55,88 @@ def main():
         elif args.verbose > 1:
             logging.basicConfig(level=logging.DEBUG)
 
-        logging.info("verbosity level = %d" % args.verbose)
+        logging.info("verbosity level = %d", args.verbose)
 
     orders = defaultdict()
-    orderHistory = list()
-    orderItems = defaultdict(list)
-    chaseTransactions = list()
-    pdfTextFiles = list()
-    locatorTransactions = defaultdict()
-    dateAmountTransactions = defaultdict()
+    order_history = []
+    order_items = defaultdict(list)
+    chase_transactions = []
+    pdf_text_files = []
+    locator_transactions = defaultdict()
+    date_amount_transactions = defaultdict()
+    glob_file_names = []
+    chase_transactions_filename = ""
 
-    globFileNames = list()
     for item in args.filenames:
-        globFileNames += glob.glob(item)
-    for fileName in globFileNames:
-        logging.debug("file %s" % fileName)
-        if os.path.splitext(fileName)[1] == '.pdf':
+        glob_file_names += glob.glob(item)
+    for filename in glob_file_names:
+        logging.debug("file %s", filename)
+        if os.path.splitext(filename)[1] == '.pdf':
             logging.info("file is pdf")
-            subprocess.run(["pdftotext", "-table", fileName])
-            pdfTextFiles.append("%s.txt" % os.path.splitext(fileName)[0])
-            chaseTransactionsFileName = os.path.splitext(fileName)[0]
+            subprocess.run(["pdftotext", "-table", "-enc", "UTF-8", filename], check=False)
+            pdf_text_files.append(f"{os.path.splitext(filename)[0]}.txt")
+            chase_transactions_filename = os.path.splitext(filename)[0]
         else:
-            with open(fileName, 'r', encoding="utf-8-sig") as fd:
-                fileBuff = fd.read()
-                reader = csv.DictReader(fileBuff.splitlines())
+            with open(filename, 'r', encoding="utf-8-sig") as fd:
+                file_buff = fd.read()
+                reader = csv.DictReader(file_buff.splitlines())
                 headers = reader.fieldnames
                 if 'payments' in headers:  # History Reporter plugin orders
-                    logging.info("file %s contains History Reporter orders" % fileName)
-                    orderHistory = reader
-                    for order in orderHistory:
+                    logging.info("file %s contains History Reporter orders", filename)
+                    order_history = reader
+                    for order in order_history:
                         orders[order['order id']] = order
                 elif 'item url' in headers:  # History Reporter plugin items
-                    logging.info("file %s contains History Reporter items" % fileName)
+                    logging.info("file %s contains History Reporter items", filename)
                     items = reader
                     for item in items:
-                        orderItems[item['order id']].append(item)
+                        order_items[item['order id']].append(item)
                 elif 'Post Date' in headers:  # Chase transactions .csv
-                    logging.info("file %s contains credit card Transaction and Post Dates" % fileName)
-                    chaseTransactions = reader
-                    for transact in chaseTransactions:
-                        match = re.search(r'^(AMZN|Amazon\.com|Prime Video|AMAZON).*', transact['Description'])
+                    logging.info("file %s contains credit card Transaction and Post Dates",
+                                 filename)
+                    chase_transactions = reader
+                    for transact in chase_transactions:
+                        match = re.search(r'^(AMZN|Amazon\.com|Prime Video|AMAZON).*',
+                                          transact['Description'])
                         if match:
-                            match2 = re.search(r'^(AMZN|Amazon\.com|Prime Video|AMAZON).*\*(\w+)', transact['Description'])
+                            match2 = re.search(r'^(AMZN|Amazon\.com|Prime Video|AMAZON).*\*(\w+)',
+                                               transact['Description'])
                             transact['Amount'] = str(-float(transact['Amount']))
                             if match2:
-                                amznLocator = match2.group(2)
-                                logging.info("amznLocator %s %s %s %s %s" % (amznLocator, transact['Transaction Date'], transact['Post Date'], transact['Description'], transact['Amount']))
-                                locatorTransactions[amznLocator] = transact
+                                amzn_locator = match2.group(2)
+                                logging.info("amznLocator %s %s %s %s %s", amzn_locator,
+                                             transact['Transaction Date'], transact['Post Date'],
+                                             transact['Description'], transact['Amount'])
+                                locator_transactions[amzn_locator] = transact
                             else:
-                                logging.info("date amount %s %s %s" % (("%s %s" % (transact['Transaction Date'], transact['Amount'])), transact['Post Date'], transact['Description']))
-                                dateAmountTransactions["%s %s" % (transact['Transaction Date'], transact['Amount'])] = transact
+                                logging.info("date amount %s %s %s",
+                                             f"{transact['Transaction Date']} {transact['Amount']}",
+                                             transact['Post Date'], transact['Description'])
+                                date_amount = f"{transact['Transaction Date']} {transact['Amount']}"
+                                date_amount_transactions[date_amount] = transact
                         else:
                             continue
                 else:
-                    sys.stderr.write("Error - Invalid csv file " + fileName + ".  Not items or shipments\n")
+                    sys.stderr.write(
+                        "Error - Invalid csv file " + filename + ".  Not items or shipments\n")
 
-    outFiles = dict()
+    out_files = {}
     dialect = csv.excel
     dialect.lineterminator = '\n'
 
-    for pdfTextFile in pdfTextFiles:
-        logging.debug("reading %s" % pdfTextFile)
-        with open(pdfTextFile) as f:
-            origFileBuff = f.read().replace('`', '')
+    for pdf_text_file in pdf_text_files:
+        logging.debug("reading %s", pdf_text_file)
+        with open(pdf_text_file, encoding="utf-8") as f:
+            orig_file_buff = f.read().replace('`', '')
             match = re.search(r'^\s*Opening/Closing Date\s+(\d+)/\d+/(\d+) - \d+/\d+/(\d+)',
-                              origFileBuff, re.M)  # @UndefinedVariable
+                              orig_file_buff, re.M)  # @UndefinedVariable
             if match:
-                openMonth = match[1]
-                openYear = match[2]
-                closeYear = match[3]
-                logging.debug("Opening/Closing %s %s %s" % (openMonth, openYear, closeYear))
+                open_month = match[1]
+                open_year = match[2]
+                close_year = match[3]
+                logging.debug("Opening/Closing %s %s %s", open_month, open_year, close_year)
             # Clean up the filebuff to
-            fileBuff = ""
+            file_buff = ""
 
             # regex patterns
             date_pattern = r'^\s*(\d\d)/(\d\d)\s+'
@@ -127,73 +146,77 @@ def main():
             order_pattern = r'\s+Order Number\s+(\S+-\d+-\d+)'
 
             # get rid of any extra stuff like page breaks etc.
-            fileBuff = "\n".join(
-                line for line in origFileBuff.split('\n')
+            file_buff = "\n".join(
+                line for line in orig_file_buff.split('\n')
                     if re.search(
                         f'{date_pattern}{descript_pattern}{withdraw_pattern}|{order_pattern}',
                         line)
             )
 
-            logging.debug(fileBuff)
+            logging.debug(file_buff)
             matches = re.findall(
                 rf'{date_pattern}{descript_pattern}{withdraw_pattern}$\s+{order_pattern}',
-                fileBuff, re.M)
+                file_buff, re.M)
             for match in matches:
                 row = dict()
                 row['Account'] = 'Prime Visa'
-                if match[0] == openMonth:
-                    row['Transaction Date'] = '%s/%s/20%s' % (match[0], match[1], openYear)
+                if match[0] == open_month:
+                    row['Transaction Date'] = f'{match[0]}/{match[1]}/20{open_year}'
                 else:
-                    row['Transaction Date'] = '%s/%s/20%s' % (match[0], match[1], closeYear)
-                origDescription = match[2]
+                    row['Transaction Date'] = f'{match[0]}/{match[1]}/20{close_year}'
+                orig_description = match[2]
                 row['Post Date'] = row['Transaction Date']
                 row['Description'] = 'Amazon'
                 row['Withdrawal'] = match[4]
-                row['Notes'] = 'Order Number %s' % (match[5])
+                row['Notes'] = f'Order Number {match[5]}'
 
-                match2 = re.search(rf'^{amazon_pattern}\*(\w+)', origDescription)
+                match2 = re.search(rf'^{amazon_pattern}\*(\w+)', orig_description)
                 if match2:
-                    amznLocator = match2.group(2)
-                    logging.info("amznLocator %s" % amznLocator)
-                    row['Notes'] += ' %s' % (amznLocator)
-                    transact = locatorTransactions.get(amznLocator)
+                    amzn_locator = match2.group(2)
+                    logging.info("amznLocator %s", amzn_locator)
+                    row['Notes'] += f' {amzn_locator}'
+                    transact = locator_transactions.get(amzn_locator)
                 else:
-                    transact = dateAmountTransactions.get("%s %s" % (row['Transaction Date'], row['Withdrawal']))
+                    date_amount = f'{row["Transaction Date"]} {row["Withdrawal"]}'
+                    transact = date_amount_transactions.get(date_amount)
 
                 if transact:
                     row['Post Date'] = transact['Post Date']
-                    row['Notes'] += ' %s' % transact['Transaction Date']
+                    row['Notes'] += f' {transact['Transaction Date']}'
                 else:
-                    sys.stderr.write("Error - tansaction missing " + "%s %s " % (row['Transaction Date'], row['Withdrawal']) + origDescription + "\n")
+                    date_amount = f'{row["Transaction Date"]} {row["Withdrawal"]}'
+                    sys.stderr.write(f"Error - transaction missing {date_amount}\n")
 
                 # Old Amazon Order Report
-                for item in orderItems[match[5]]:
-                    # Note: if the quanity is > 1 it is possible 1 item can come
+                for item in order_items[match[5]]:
+                    # Note: if the quality is > 1 it is possible 1 item can come
                     # in multiple shipments in that case the item total will be
                     # the sum of the total charges.
                     quantity = ''
                     if int(item['Quantity']) > 1:
-                        quantity = '%sx ' % item['Quantity']
-                    row['Description'] += ' %s%s(%s)' % (quantity, item['Title'], item['Item Total'])
-                    row['Notes'] += ' %s' % (item['Category'])
+                        quantity = f'{item["Quantity"]}x '
+                    row['Description'] += f' {quantity}{item["Title"]}({item["Item Total"]})'
+                    row['Notes'] += f' {item['Category']}'
 
                 # :TODO: Put New logic here
                 if match[5] in orders:
                     order = orders[match[5]]
-                    row['Description'] += ' %s' % (order['items'])
+                    row['Description'] += f' {order['items']}'
 
-                row['Description'] += ' %s' % match[5]
-                logging.debug("%s %s %s %s %s %s\n  %s" % (row['Account'], row['Transaction Date'], row['Post Date'], row['Description'], row['Withdrawal'], row['Notes'], origDescription))
+                row['Description'] += f' {match[5]}'
 
-                if row['Account'] in outFiles:
-                    writer = csv.DictWriter(outFiles[row['Account']], fieldnames=row.keys(), dialect=dialect)
+                logging.debug("%s %s %s %s %s %s\n  %s", row['Account'], row['Transaction Date'],
+                              row['Post Date'], row['Description'], row['Withdrawal'], row['Notes'],
+                              orig_description)
+                if row['Account'] in out_files:
+                    writer = csv.DictWriter(out_files[row['Account']], fieldnames=row.keys(),
+                                            dialect=dialect)
                 else:
-                    keepcharacters = (' ', '.', '_')
-                    safeAccount = "".join(c for c in row['Account'] if c.isalnum() or c in keepcharacters).rstrip()
-                    csvfilename = "%s-%s-out.csv" % (chaseTransactionsFileName, safeAccount)
-                    logging.debug("%s - file - %s\n" % (row['Account'], csvfilename))
+                    safe_account = re.sub(r'[^a-zA-Z0-9 ._-]', '', row['Account']).rstrip()
+                    csvfilename = f"{chase_transactions_filename}-{safe_account}-out.csv"
+                    logging.debug("%s - file - %s\n", row['Account'], csvfilename)
                     csvfile = open(csvfilename, 'w', encoding="utf8")
-                    outFiles[row['Account']] = csvfile
+                    out_files[row['Account']] = csvfile
                     writer = csv.DictWriter(csvfile, fieldnames=row.keys(), dialect=dialect)
                     writer.writeheader()
 
